13276776f3237e44f716d5ebb37f1739f0500cba
dashzou <fxfddfno@gmail.com>
2019-11-01 15:14:53 +0200

    hide email not owner

--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -1,4 +1,15 @@
-const User = {};
+import Auth from '../utils/Auth';
+
+const User = {
+  email(parent, args, { request }, info) {
+    const userId = Auth(request, false);
+    if (userId && userId === parent.id) {
+      return parent.email;
+    } else {
+      return null;
+    }
+  }
+};
 
 //! now we did not have that because we add info in prisma.query
 export { User as default };
