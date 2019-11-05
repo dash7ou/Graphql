f07b09804975c4a19daf74814b2671d3379578f6
dashzou <fxfddfno@gmail.com>
2019-11-05 18:54:47 +0200

    add some test user.js

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -1,1 +1,26 @@
-test('this is my first test case', () => {});
+import { getFirstName, isValidPassword } from '../src/utils/user';
+
+test('Should return first name when given full name', () => {
+  const firstName = getFirstName('Mohammed M R Zourob');
+  expect(firstName).toBe('Mohammed');
+});
+
+test('Should return first name when given first name', () => {
+  const firstName = getFirstName('Mohammed');
+  expect(firstName).toBe('Mohammed');
+});
+
+test('should reject password shorter than 8 character', () => {
+  const isValid = isValidPassword('abc1234');
+  expect(isValid).toBe(false);
+});
+
+test('should reject password contains word password', () => {
+  const isValid = isValidPassword('password12345');
+  expect(isValid).toBe(false);
+});
+
+test('should correctly validate a valid password', () => {
+  const isValid = isValidPassword('mohammed2344');
+  expect(isValid).toBe(true);
+});
